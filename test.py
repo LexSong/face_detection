737404b87c293543b7cb3558db163da25b8f9a42
Lex Song <LexSong.tw@gmail.com>
2018-05-31 17:28:17 +0800

    Refactor test.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-
 from api import S3FD
 
 model = "models/s3fd_convert.pth"
@@ -8,17 +6,17 @@ s3fd = S3FD(model)
 
 cap = cv2.VideoCapture(0)
 while(True):
-    ret, img = cap.read()
+    ret, frame = cap.read()
+    boxes = s3fd.detect(frame, 0.5)
+    boxes = boxes[:, 0:4].astype(int)
 
-    imgshow = np.copy(img)
-    bboxlist = s3fd.detect(img, 0.5)
-
-    for b in bboxlist:
-        x1, y1, x2, y2, s = b
-        cv2.rectangle(imgshow, (int(x1), int(y1)),
-                      (int(x2), int(y2)), (0, 255, 0), 1)
-
-    cv2.imshow('test', imgshow)
+    figure = frame.copy()
+    for box in boxes:
+        p1 = tuple(box[0:2])
+        p2 = tuple(box[2:4])
+        color = (0, 255, 0)
+        cv2.rectangle(figure, p1, p2, color, 1)
+    cv2.imshow('', figure)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
