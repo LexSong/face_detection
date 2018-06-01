bec5c78f62f10fa50fb68c28f1ce395c6bbe8cbc
Lex Song <LexSong.tw@gmail.com>
2018-06-01 14:51:28 +0800

    Add Video wrapper class to test.py

--- test.py
+++ test.py
@@ -1,12 +1,36 @@
 import cv2
+import sys
+from tqdm import tqdm
+
 from api import S3FD
+
+
+class Video(object):
+    def __init__(self, filename):
+        self.cap = cv2.VideoCapture(filename)
+        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def iter_frames(self):
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            yield frame
+
 
 model = "models/s3fd_convert.pth"
 s3fd = S3FD(model)
 
-cap = cv2.VideoCapture(0)
-while(True):
-    ret, frame = cap.read()
+video = Video(sys.argv[1])
+
+resize_shape = (640, int(640*video.frame_height/video.frame_width))
+
+iter_frames = tqdm(video.iter_frames(), total=video.frame_count, ascii=True)
+
+for frame in iter_frames:
+    frame = cv2.resize(frame, resize_shape, interpolation=cv2.INTER_AREA)
     boxes = s3fd.detect(frame, 0.5)
     boxes = boxes[:, 0:4].astype(int)
 
